8ec9f940a2b8e540bdbb50049f474f234189557d
ritvik puranik <ritvikpuranik@gmail.com>
2025-01-14 09:13:24 +0530

    basic structure created

---
+++ personal-portfolio/schemaTypes/index.js
@@ -0,0 +1,9 @@
+import portfolio from './portfolio';
+import service from './service';
+import testimonial from './testimonial';
+
+export const schemaTypes = [
+  portfolio,
+  service,
+  testimonial,
+]
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,101 @@
-import './App.css';
-import Navbar from './components/Navbar.jsx';
-import Intro from './components/Intro.jsx';
-import Projects from './components/Projects.jsx';
-import Ventures from './components/Ventures.jsx';
-import ReachOut from './components/ReachOut.jsx';
+import React, { useEffect, useState } from "react"
+
+// Components
+import Nav from "components/Nav/Nav"
+import Home from "components/Home/Home"
+import Menu from "components/Menu/Menu"
+import Portfolio from "components/Portfolio"
+import Contact from "components/Contact/Contact"
+import Services from "components/Services/Services"
+import Testimonials from "components/Testimonials/Testimonials"
+import Footer from "components/Footer/Footer"
+// Sanity
+import sanityClient from "client"
+
+// Animation
+import Aos from "aos"
+import "aos/dist/aos.css"
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
+  const [projects, setProjects] = useState([])
+  const [services, setServices] = useState([])
+
+  const [testimonials, setTestimonials] = useState([])
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type in ["testimonial", "portfolio","service"]]{
+          _type == "testimonial" => {
+            id,
+            name,
+            text,
+            country,
+            url
+          },
+          _type == "portfolio" => {
+            id,
+            title,
+            tagline,
+            category,
+            liveUrl,
+            repositoryUrl,
+            img{
+              asset->{url}
+            },
+          },
+          _type == "service" => {
+            title,
+            tags,
+            icon{
+              asset->{url}
+            },
+          },
+        }
+        `
+      )
+      .then((data) => {
+        let tempTestimonials = []
+        let tempProjects = []
+        let tempServices = []
+
+        data.map((doc) => {
+          if (doc.country) {
+            // It is a testimonial
+            tempTestimonials.push(doc)
+          } else if (doc.liveUrl) {
+            // It is a project
+            tempProjects.push(doc)
+          } else {
+            tempServices.push(doc)
+          }
+
+          return null
+        })
+        setTestimonials(tempTestimonials)
+        setProjects(tempProjects)
+        setServices(tempServices)
+      })
+      .catch(console.error)
+  }, [])
+
+  useEffect(() => {
+    Aos.init({ duration: 2000, offset: 10 })
+  }, [])
+
   return (
-  <div>
-    <header>
-      <Navbar />
-    </header>
-    <Intro />
-    <div className="b-example-divider"></div>
-    <Projects />
-    <div className="b-example-divider"></div>
-    <Ventures />
-    <div className="b-example-divider"></div>
-    <ReachOut />
-  </div>
-  );
+    <div className="app-container">
+      <Nav menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home />
+      <Services services={services} />
+      <Portfolio projects={projects} />
+      <Testimonials testimonials={testimonials} />
+      <Contact />
+      <Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
