f32ea9015a41b9aa52dac1c9fd635f9dbf89c221
ritvik puranik <ritvikpuranik@gmail.com>
2025-01-16 12:18:33 +0530

    removed personal portfolio

--- personal-portfolio/schemaTypes/index.js
+++
@@ -1,9 +0,0 @@
-import portfolio from './portfolio';
-import service from './service';
-import openSource from './openSource';
-
-export const schemaTypes = [
-  portfolio,
-  service,
-  openSource
-]
